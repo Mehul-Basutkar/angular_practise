5c443bb9a6bc09a40f736fca69e1c0f91d8e9c72
Mehul-Basutkar <mehulbasutkar@gmail.com>
2021-07-09 00:11:22 +0530

    draw form with bootstrap

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgotComponent } from './forgot/forgot.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:LoginComponent},
+  { path:'forgot', component:ForgotComponent},
+  { path:'signup', component:SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
